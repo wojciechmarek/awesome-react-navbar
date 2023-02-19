1ad9b7319c60cc5d8cfcaa4d8527302c17575a41
Wojciech Marek <marek.wojciech@outlook.com>
2023-02-19 23:25:04 +0100

    feat: add react markdown library

--- src/components/views/Changelog.tsx
+++ src/components/views/Changelog.tsx
@@ -1,9 +1,9 @@
+import ReactMarkdown from "react-markdown";
+
 export const Changelog = () => {
   return (
-    <main className="bg-white dark:bg-blue-900 h-[calc(100vh-5em)] flex justify-center items-center">
-      <h1 className="text-2xl font-bold text-gray-900 dark:text-white text-center text-5xl">
-        Changelog
-      </h1>
+    <main className="bg-white dark:bg-blue-900 h-[calc(100vh-5em)] flex justify-center">
+      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
     </main>
   );
 };
