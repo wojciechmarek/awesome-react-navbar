1be6eb5fc210d2cac0bd57d50d17c343eb920d2f
Wojciech Marek <marek.wojciech@outlook.com>
2023-02-19 16:47:43 +0100

    refactor: change files structure

--- src/components/views/Changelog.tsx
+++ src/components/views/Changelog.tsx
@@ -1,8 +1,8 @@
-export const Blogs = () => {
+export const Changelog = () => {
   return (
     <main className="bg-white dark:bg-blue-900 h-[calc(100vh-5em)] flex justify-center items-center">
       <h1 className="text-2xl font-bold text-gray-900 dark:text-white text-center text-5xl">
-        Blogs
+        Changelog
       </h1>
     </main>
   );
