8b53537c20403855e340e6c778bcc6585dde068d
Wojciech Marek <marek.wojciech@outlook.com>
2023-02-19 23:36:00 +0100

    feat: add extra tailwind library to deal with markdown

--- src/components/views/Changelog.tsx
+++ src/components/views/Changelog.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 export const Changelog = () => {
+  const [content, setContent] = useState("");
+
+  fetch(
+    "https://raw.githubusercontent.com/wojciechmarek/AwesomeReactNavbar/master/CHANGELOG.md"
+  )
+    .then((res) => res.text())
+    .then((text) => setContent(text));
+
   return (
     <main className="bg-white dark:bg-blue-900 h-[calc(100vh-5em)] flex justify-center">
-      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      <div className="w-5/6 md:w-3/6">
+        <ReactMarkdown className="prose prose-headings:text-neutral-300 prose-li:text-neutral-400">
+          {content}
+        </ReactMarkdown>
+      </div>
     </main>
   );
 };
