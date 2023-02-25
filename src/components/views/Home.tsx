8d9ee12800ead554d76e6b4020a4b3780b7e4c78
Wojciech Marek <marek.wojciech@outlook.com>
2023-02-25 17:15:02 +0100

    feat: set up theme toggler

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 
 export const Home = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
   const onChangeThemeClick = () => {
-    // setTheme(theme === "light" ? "dark" : "light");
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+
+    if (newTheme === "dark") {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    }
   };
 
   return (
@@ -14,7 +24,7 @@ export const Home = () => {
         onClick={onChangeThemeClick}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10"
       >
-        Change Theme
+        Change Theme to: {theme === "light" ? "dark" : "light"}
       </button>
     </main>
   );
