309f80b3d09ea766ada8397e6f0f15d9868b1434
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-04 19:35:58 +0100

    feat: add option to see pwa in the dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
