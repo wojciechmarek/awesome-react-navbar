80e2e20657e2faf12b60468a8d2632e4e2e62e7d
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-13 21:03:00 +0100

    feat: set logo link to main page

--- src/components/ui-elements/CompanyLogo/CompanyLogo.tsx
+++ src/components/ui-elements/CompanyLogo/CompanyLogo.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const CompanyLogo = () => {
   return (
-    <a href="#" className="flex items-center">
+    <Link to="/" className="flex items-center">
       <span className="self-center text-xl font-bold text-gray-800 whitespace-nowrap dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
         My Company
       </span>
-    </a>
+    </Link>
   );
 };
