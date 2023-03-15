8d6b9bbf49b45cac8e2e6b60fb9db737640f6f89
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-15 19:59:00 +0100

    feat: add mobile menu overflow

--- src/components/ui-elements/MobileMenu/MobileMenu.tsx
+++ src/components/ui-elements/MobileMenu/MobileMenu.tsx
@@ -19,7 +19,7 @@ export const MobileMenu = ({ menuLinks }: MobileMenuProps) => {
 
   return (
     <div className="absolute w-[calc(100vw-2.5em)] h-[calc(100vh-6em)] top-20 left-5 rounded-lg bg-slate-50 dark:bg-gray-900 shadow-[rgba(0,_0,_0,_0.24)_0px_0px_40px] shadow-slate-400 dark:shadow-slate-700">
-      <div className="flex flex-col h-full m-8">
+      <div className="flex flex-col h-[calc(100%-4em)] m-8 overflow-auto">
         <ul>
           {menuLinks.map((link, index) => (
             <div className="relative group" key={link.name}>
